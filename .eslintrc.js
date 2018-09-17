29e173c91dbabfc2e3c2ca1eb2a756d6a5f92192
Mavriiick42 <clownclowed2@gmail.com>
2018-09-17 22:35:08 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'airbnb',
+  parserOptions: { ecmaVersion: 6 },
+  env: { es6: true },
+  ecmaFeatures: {
+    jsx: true,
+  },
+  rules: {
+    'react/jsx-filename-extension': 0,
+  },
+};
