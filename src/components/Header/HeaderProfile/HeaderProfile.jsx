a2efb25484f25f062368a138c7006aaf4d9fb9df
Mavrick <Mavrick91@users.noreply.github.com>
2018-09-19 13:19:34 +0800

    15-create-test-subcomponent-header

--- src/components/Header/HeaderProfile/HeaderProfile.jsx
+++ src/components/Header/HeaderProfile/HeaderProfile.jsx
@@ -4,11 +4,19 @@ import React, { Component } from 'react';
 
 import mignonne from '../../../mignonne.jpg';
 
-export default class HeaderProfile extends Component<{}> {
+type Props = {
+  cmsHeader: HeaderType,
+};
+
+export default class HeaderProfile extends Component<Props> {
   render() {
+    const {
+      cmsHeader: { help },
+    } = this.props;
+
     return (
       <div className="information__profile">
-        <div className="profile__help">AIDE</div>
+        <div className="profile__help">{help}</div>
         <div className="profile__dropdown">
           <img className="dropdown__image" src={mignonne} alt="" />
         </div>
--- src/components/Header/HeaderProfile/HeaderProfile.spec.js
+++ src/components/Header/HeaderProfile/HeaderProfile.spec.js
@@ -4,9 +4,20 @@ import { expect } from 'chai';
 import HeaderProfile from './HeaderProfile';
 
 describe('<HeaderProfile />', () => {
-  const wrapper = shallow(<HeaderProfile />);
+  const props = {
+    cmsHeader: {
+      help: 'AIDE',
+    },
+  };
+  const wrapper = shallow(<HeaderProfile {...props} />);
 
   it('should render component', () => {
     expect(wrapper).to.have.lengthOf(1);
+  });
+
+  it('should have the correct props', () => {
+    expect(wrapper.instance().props.cmsHeader).to.deep.equal({
+      help: 'AIDE',
+    });
   });
 });
