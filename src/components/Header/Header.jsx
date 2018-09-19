4a3d1e421c1f3e8017226899cceec54abb472476
Mavriiick42 <clownclowed2@gmail.com>
2018-09-19 12:25:13 +0800

    setup graphcool

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,17 +5,25 @@ import React, { Component } from 'react';
 import HeaderProfile from './HeaderProfile';
 import HeaderEventAttendees from './HeaderEventAttendees';
 
-type Props = {};
+type Props = {
+  cmsHeader: HeaderType,
+  attendeesCount: number,
+};
 
 export default class Header extends Component<Props> {
   render() {
+    const { attendeesCount, cmsHeader } = this.props;
+
     return (
       <div className="header">
         <div className="header__logo">h</div>
         <div className="header__information">
           <div className="information__wrapper">
-            <HeaderEventAttendees />
-            <HeaderProfile />
+            <HeaderEventAttendees
+              attendeesCount={attendeesCount}
+              cmsHeader={cmsHeader}
+            />
+            <HeaderProfile cmsHeader={cmsHeader} />
           </div>
         </div>
       </div>
